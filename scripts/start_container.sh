2bd8094c881b0f966680c3c0a928b3ed2f16c906
JhamelT <102842426+JhamelT@users.noreply.github.com>
2025-06-22 19:08:07 -0400

    Delete scripts directory

--- scripts/start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker Image from Docker Hub - Copy Docker Pull Command
-docker pull jhamelthorne/myapp
-
-# Run the Docker image as container
-docker run -d -p 5000:5000 jhamelthorne/myapp
