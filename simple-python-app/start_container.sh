76f7af82a731d3a8dc7362977fa9b3b92216f84b
JhamelT <102842426+JhamelT@users.noreply.github.com>
2025-06-25 19:53:16 -0400

    Update start_container.sh

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker Image from Docker Hub - Copy Docker Pull Command
+# Pull the Docker Image from Docker Hub - Copy Docker Pull Command 
 docker pull jhamelthorne/myapp
 
 # Run the Docker image as container
